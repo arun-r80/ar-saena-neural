b77ebb842c27a80da22c79aaaefd0b92e243c276
Arun Ramamurthy <Arun.Ramamurthy@asggroup.com.au>
2023-03-16 10:58:49 +1100

    Add all files, may be we cannot work with Windows Anymore

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ training_data_file = os.path.join(os.getcwd())#, "data",training_data_redimensio
 # handwritten_digits.train(500)
 
 handwritten_digits = neural2.Neural_2(training_data_file, [784,100, 9], 60000)
-handwritten_digits.train(500)
+handwritten_digits.train(5)
 
 
 
